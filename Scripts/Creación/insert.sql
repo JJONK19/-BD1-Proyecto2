735d87bb2622d461523cdb810424a9b0749c005a
Javier Aguilar <josuejaviera19@gmail.com>
2023-10-17 18:03:58 -0600

    Insercion de Datos

--- Scripts/Creación/insert.sql
+++ Scripts/Creación/insert.sql
@@ -1,10 +1,40 @@
 -- LLENADO DE TABLAS 
 
 -- Carrera
-INSERT INTO carrera (id, nombre)
-VALUES (0, 'Area Comun');
+INSERT INTO carrera (nombre)
+VALUES ('Area Comun');
+
+UPDATE carrera
+SET id = 0
+WHERE nombre = 'Area Comun';
 
 ALTER TABLE carrera AUTO_INCREMENT = 1;
 
-LOAD DATA INFILE 'tu_archivo.csv' INTO TABLE tu_tabla
-FIELDS TERMINATED BY ',' (columna1, columna2, ...);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carreras.csv' 
+INTO TABLE carrera
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES
+(nombre);
+
+-- Curso
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/cursos.csv'
+INTO TABLE curso
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES
+(codigo, nombre, creditos_necesarios, creditos_otorgados, obligatorio, carrera);
+
+-- Docente
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/docentes.csv'
+INTO TABLE docente
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES
+(siif, dpi, nombres, apellidos, fecha_nacimiento, correo, telefono, direccion)
+SET fecha_registro = CURRENT_DATE;
+
+-- Estudiante
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/estudiantes.csv'
+INTO TABLE estudiante
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES
+(carnet, nombres, apellidos, fecha_nacimiento, correo, telefono, direccion, dpi, carrera)
+SET fecha_registro = CURRENT_DATE;
